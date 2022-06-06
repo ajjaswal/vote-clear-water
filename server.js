d5fbb845bce04c95d9461029a068c3eee9bfc155
AJ Jaswal <jaswalakshay@yahoo.com>
2022-06-06 10:21:18 -0400

    connecting to the database

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World'
-//     });
-// });
+// Connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: '!Baku7boss',
+        database: 'election'
+    },
+    console.log('Conected to the election database.')
+);
+
+
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
