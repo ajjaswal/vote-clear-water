d205792f703ff550e088782223f695d176af5e87
AJ Jaswal <jaswalakshay@yahoo.com>
2022-06-06 10:00:17 -0400

    set up port

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
-const express = requrie('express')
+const express = require('express')
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Express middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'Hello World'
+//     });
+// });
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
